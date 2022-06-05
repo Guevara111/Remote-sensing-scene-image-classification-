7985ec48629dbc0c2097eb87a341a2c8ce0d7080
高翔 <55071222+Gao-Xiang-01@users.noreply.github.com>
2022-06-05 10:52:52 +0800

    Update svmfenlei.m

--- code/steps3特征融合及分类/svmfenlei.m
+++ code/steps3特征融合及分类/svmfenlei.m
@@ -1,5 +1,5 @@
 function [label,score,accuracy]=svmfenlei(trainfeature,labeltrainnum,testfeature,labeltestnum)
-aqishi =fitcecoc(trainfeature,labeltrainnum,'Learners','svm');
+aqishi =fitcecoc(trainfeature,labeltrainnum,'Learners','svm','OptimizeHyperparameters','auto','HyperparameterOptimizationOptions',struct('Optimizer','bayesopt'));
 [label,score] = predict(aqishi,testfeature);
 accuracy = mean(label ==labeltestnum);
 clear svmfenlei
